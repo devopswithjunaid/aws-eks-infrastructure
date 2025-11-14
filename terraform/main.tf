52ca32654c22838a4977fef0afa74b943a9205be
devopswithjunaid <devopswithjunaid@gmail.com>
2025-11-15 01:06:08 +0500

    Reorganize: Move VPN module to terraform/modules directory
    - Moved vpn/ to terraform/modules/vpn/ for better organization
    - Updated main.tf module source path
    - Updated README.md project structure
    - All modules now consistently located in terraform/modules/

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,9 +59,9 @@ module "node_group" {
   depends_on = [module.eks_cluster]
 }
 
-# VPN Module - Now from vpn folder
+# VPN Module
 module "vpn" {
-  source = "../vpn"
+  source = "./modules/vpn"
   
   project_name     = var.project_name
   vpc_id           = module.vpc.vpc_id
